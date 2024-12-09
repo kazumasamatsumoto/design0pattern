4fd5ae8e615b282dd13b04e58cbb7ba3f11c88c5
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2024-12-09 10:59:08 +0900

    liskovの悪いパターンの実装

---
+++ 03-solid-principle/liskovSubstitution/badExample.ts
@@ -0,0 +1,45 @@
+export {};
+
+export class Rectangle {
+  width = 0;
+  height = 0;
+
+  setWidth(width: number) {
+    this.width = width;
+  }
+
+  setHeight(height: number) {
+    this.height = height;
+  }
+
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+export class Square extends Rectangle {
+  setWidth(width: number) {
+    super.setWidth(width);
+    super.setHeight(width);
+  }
+
+  setHeight(height: number) {
+    super.setWidth(height);
+    super.setHeight(height);
+  }
+}
+
+export function f(r: Rectangle, width: number, height: number): number {
+  r.setWidth(width);
+  r.setHeight(height);
+  return r.getArea();
+}
+
+function run() {
+  const r1: Rectangle = new Rectangle();
+  const r2: Rectangle = new Square();
+  console.log(f(r1, 3, 4));
+  console.log(f(r2, 3, 4));
+}
+
+run();
